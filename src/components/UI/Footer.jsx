67c9c4870c9a724bc7df9611301beb413fab9fc1
Kutubuddin Ansari <kutubofficial599@gmail.com>
2025-02-07 20:00:17 +0530

    this is final commit and project will updated.

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,24 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className=" flex mx-auto min-h-30  w-5xl text-center justify-center gap-4 p-10 bg-transparent">
-      
-      <p className="text-gray-300 text-lg mt-20 ">
-        &copy; 2022 World Atlas. All rights reserved.
+    <div className="flex flex-col items-center text-center p-10  bg-gray-700">
+      <h2 className="text-2xl text-green-500 font-semibold mb-4">
+        🌍 World Atlas
+      </h2>
+
+      <div className="flex gap-4 text-gray-400 text-xl mb-4">
+        <a
+          href="https://www.facebook.com/sahzaadekutub.kutub/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook className="hover:text-green-500 transition" />
+        </a>
+        <a
+          href="https://twitter.com/itxaibak"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaTwitter className="hover:text-green-400 transition" />
+        </a>
+        <a
+          href="https://instagram.com/in/only.kutub"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram className="hover:text-green-500 transition" />
+        </a>
+        <a
+          href="https://linkedin.com/in/kutubofficial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="hover:text-green-700 transition" />
+        </a>
+      </div>
+
+      <p className="text-gray-400 text-sm">
+        &copy; {new Date().getFullYear()} World Atlas. All rights reserved.
       </p>
-      <ul className="flex gap-2 text-gray-400 mt-20 ">
-        <li>
-          <Link to="https://atlaspolicy.com/">Privacy Policy</Link>
-        </li>
-        <li>
-          <Link to="https://www.atlashxm.com/terms-and-conditions">Terms & Condition</Link>
-        </li>
-        <li>
-          <Link to="/contacts">Contact Us</Link>
-        </li>
-      </ul>
     </div>
   );
 };
